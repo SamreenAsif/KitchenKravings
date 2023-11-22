b1cab41dda09483c3c5f9dabd4fa48414825dd45
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-11-22 06:54:30 +0500

    bottom Nav

---
+++ app/src/main/java/com/example/recipebook/BottomNavGraph.kt
@@ -0,0 +1,25 @@
+package com.example.recipebook
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+@Composable
+fun BottomNavGraph(navController: NavHostController , modifier : Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.Home.route
+    ) {
+        composable(route = BottomBarScreen.Home.route) {
+            HomeScreen()
+        }
+        composable(route = BottomBarScreen.Profile.route) {
+            ProfileScreen()
+        }
+        composable(route = BottomBarScreen.Settings.route) {
+            SettingScreen()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recipebook/MainScreen.kt
@@ -0,0 +1,153 @@
+package com.example.recipebook
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color(android.graphics.Color.parseColor("#f06d0a")), //Card background color
+                    titleContentColor = androidx.compose.ui.graphics.Color.White,
+                ),
+                title = {
+                    androidx.compose.material3.Text(
+                        "Hello, !",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    androidx.compose.material3.IconButton(onClick = { /* do something */ }) {
+                        androidx.compose.material3.Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    androidx.compose.material3.IconButton(onClick = { /* do something */ }) {
+                        androidx.compose.material3.Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        bottomBar = { BottomBar(navController = navController) }
+    ) { innerpadding ->
+        BottomNavGraph(navController = navController, modifier = Modifier.padding(innerpadding))
+    }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Settings,
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+//    BottomNavigation (
+//        modifier = Modifier.background(color = Color(0xFF2196F3)),
+//    ) {
+//
+//        screens.forEach { screen ->
+//            AddItem(
+//                screen = screen,
+//                currentDestination = currentDestination,
+//                navController = navController
+//            )
+//        }
+//    }
+    BottomNavigation (
+        modifier = Modifier.height(70.dp) ,
+        backgroundColor = Color(android.graphics.Color.parseColor("#f06d0a"))
+    ){
+        screens.forEach { screen ->
+            BottomNavigationItem(
+                icon = {
+                    // Set your icon based on the screen
+                    Icon(imageVector = screen.icon, contentDescription = screen.title)
+                },
+                label = { Text(text = screen.title) },
+                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                onClick = {
+                    // Handle navigation or other actions when the item is clicked
+                    navController.navigate(screen.route)
+                },
+                modifier = Modifier
+                    // Apply additional styling to the BottomNavigationItem
+                    .padding(4.dp) // Add padding as needed
+                    .fillMaxHeight(), // Adjust height as needed
+                selectedContentColor = Color.White, // Set color for the selected item
+                unselectedContentColor = Color.Gray // Set color for unselected items
+            )
+        }
+    }
+
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        onClick = {
+            navController.navigate(screen.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+    )
+}
