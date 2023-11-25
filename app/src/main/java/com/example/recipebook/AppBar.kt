00b112e8edcfc2c60fe92c581c9cb651555ccd2b
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-11-25 16:44:43 +0500

    Added Side Bar

---
+++ app/src/main/java/com/example/recipebook/AppBar.kt
@@ -0,0 +1,70 @@
+package com.example.recipebook
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.recipebook.BottomBarScreen.Home.title
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    navController : NavHostController,
+    scrollBehavior : TopAppBarScrollBehavior,
+    onNavigationIconClick : () -> Unit
+){
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    // Get the current destination
+    val currentDestination = navBackStackEntry?.destination?.route
+
+    // Determine the title based on the current destination
+    val title = when (currentDestination) {
+        BottomBarScreen.Home.route -> "Hello,Guest!"
+        BottomBarScreen.Profile.route -> BottomBarScreen.Profile.title
+        BottomBarScreen.Settings.route -> BottomBarScreen.Settings.title
+        else -> "Hello,Guest!" // Default title
+    }
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = Color(android.graphics.Color.parseColor("#f06d0a")),
+            titleContentColor = Color.White,
+        ),
+        actions = {
+            IconButton(onClick = onNavigationIconClick) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Localized description",
+                    tint =  Color.White
+                )
+            }
+        },
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.White,
+                fontSize = 20.sp,
+
+                )
+        },
+        scrollBehavior = scrollBehavior,
+    )
+}
