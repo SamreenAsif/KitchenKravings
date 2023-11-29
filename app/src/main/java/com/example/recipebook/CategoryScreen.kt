a397ee2da0b77135dc4efbd7f46cea7744530d2f
SamreenAsif <l201292@lhr.nu.edu.pk>
2023-11-29 05:30:45 +0500

    minor changes in category page

--- app/src/main/java/com/example/recipebook/CategoryScreen.kt
+++ app/src/main/java/com/example/recipebook/CategoryScreen.kt
@@ -1,7 +1,20 @@
 package com.example.recipebook
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.recipebook.data.ItemCategory
 
 
@@ -39,11 +52,53 @@ val CuisineList = listOf(
     // Add more items as needed
 )
 @Composable
-fun CategoryScreen()
+fun CategoryScreen() {
+    Surface(
+        modifier = Modifier
+            .padding(bottom = 30.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        LazyColumn(
+            contentPadding = PaddingValues(start=8.dp ,end= 8.dp ,top=30.dp ,bottom = 50.dp)
+        )
+        {
+            item {
+                ListItem(items = coursesList , "Courses",null , 130.dp)
+
+            }
+            item {
+                ListItem(items = CuisineList , "Cuisines",null , 380.dp)
+
+            }
+            item {
+                ListItem(items = CuisineList , "Cuisines",null , 380.dp)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun ListItem(
+    items :List<ItemCategory>,
+    categoryTitle : String,
+    numColumns: Int? = null,
+    maxHeight: Dp? = Dp.Unspecified,
+    min : Dp? = 110.dp)
 {
     Column() {
-        CategoryButton(images = categoryItems)
-        CategoryButton(images = coursesList)
-        CategoryButton(images = CuisineList)
+        Text(
+            text = categoryTitle,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Black,
+            modifier = Modifier.padding(
+                start = 20.dp,
+                top = 8.dp,
+                bottom = 20.dp
+            )
+        )
+        CategoryButton(items, numColumns, maxHeight,min)
     }
 }
